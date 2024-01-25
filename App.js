96c3c224b171f172204c53c6c89f3dc6d0b24313
sparklinstar <souradip1000@gmail.com>
2024-01-25 10:18:07 +0530

    added AppTextInput prop

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 import AccountScreen from "./app/screens/AccountScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
+import AppTextInput from "./app/components/AppTextInput";
 export default function App() {
+  const [firstName, setFirstName] = useState("");
+  console.log(firstName);
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <ListingsScreen />
-    </GestureHandlerRootView>
+    <Screen>
+      <AppTextInput placholder="Username" />
+    </Screen>
   );
 }
